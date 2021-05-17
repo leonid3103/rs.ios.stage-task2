212c5f47d4f55ee0f727cd0f30605d25e36e9b38
leonid3103 <leonid2015@yandex.ru>
2021-05-17 22:40:14 +0300

    Task 2 (exercise 1,2)

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,16 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    NSUInteger revers = 0;
+    for (NSUInteger i = 0; i < 8; i++)
+    {
+        if (n/2 >= 0) {
+            if(n%2){
+                revers+= pow(2, 7 - i);
+            }
+            n /= 2;
+        } else break;
+    }
+    
+    return revers;
 }
--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,16 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger count = 0;
+         NSInteger lenght = array.count;
+         for (int i = 0; i < lenght; i++) {
+             for (int j = i+1; j < lenght; j++) {
+                 if (abs([array[j] intValue] - [array[i] intValue]) == [number intValue]) {
+                     count++;
+                 }
+             }
+         }
+         return count;
 }
 
 @end
